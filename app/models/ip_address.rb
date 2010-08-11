bda6f9f3ed1ca305123750041fd3d8e51f3b9df7
Jonathan Kupferman <jkupferman@umail.ucsb.edu>
2010-08-10 20:25:47 -0700

    Added IpAddress class as a proxy for IPAddr class. Added in? IpRange method.

---
+++ app/models/ip_address.rb
@@ -0,0 +1,19 @@
+require 'ipaddr'
+
+# This class is simply a proxy for the IPAddr class
+class IpAddress
+  def initialize(ip_address)
+    @ip_address = IPAddr.new(ip_address)
+  end
+
+  def in? ip_range
+    raise ArgumentError unless ip_range.kind_of?(IpRange)
+
+    ip_range.include?(self)
+  end
+
+  private
+  def method_missing(method, *args, &block)
+    @ip_address.send(method, *args, &block)
+  end
+end
