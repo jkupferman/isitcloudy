52d5f1532ec4b42b75be7d3accb6ca4b9b264a20
Jonathan Kupferman <jkupferman@umail.ucsb.edu>
2010-08-10 22:46:13 -0700

    Added better url parsing to website class. Added tests to ensure url variations are being resolved correctly.

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,8 +1,13 @@
 require 'dnsruby'
 
 class Website
+  HTTP_PREFIX_REGEX = /https?:\/\//
+  WWW_PREFIX_REGEX = /^www\./
+
   def initialize url
-    @url = url
+    @url = url.to_s.downcase
+    @url.gsub!(HTTP_PREFIX_REGEX, "")
+    @url.gsub!(WWW_PREFIX_REGEX, "")
   end
 
   def ip_addresses
--- spec/models/website.rb
+++ spec/models/website.rb
@@ -13,12 +13,52 @@ describe Website do
     end
 
     it "should fetch the ips of facebook" do
-      @actual_ips = [ "69.63.181.12", "69.63.189.11", "69.63.189.16", "69.63.181.11"]
+      @actual_ips = ["69.63.181.12", "69.63.189.11", "69.63.189.16", "69.63.181.11"]
 
       @facebook = Website.new("facebook.com")
       @facebook.ip_addresses.should =~ @actual_ips
     end
-  end
 
+    context "with different url variations" do
+      context ""
+      before do
+        @actual_ips = ["174.129.230.90", "174.129.238.229"]
+        @actual_url = "rightscale.com"
+      end
+
+      context "before actual url" do
+        def check_url url
+          @rightscale = Website.new(url)
+          @rightscale.ip_addresses.should =~ @actual_ips
+        end
+
+        it "should get ips when www is added to the url" do
+          check_url("www." + @actual_url)
+        end
+
+        it "should get ips when http://www is added to the url" do
+          check_url("http://www." + @actual_url)
+        end
+
+        it "should get ips when https://www is added to the url" do
+          check_url("https://www." + @actual_url)
+        end
+
+        it "should get ips when https://www is added to the url" do
+          check_url("https://www." + @actual_url)
+        end
+
+        it "should get ips when http:// is added to the url" do
+          check_url("http://" + @actual_url)
+        end
+      end
+
+      context "with .com suffixes" do
+        it "should work with urls that have .edu suffixes"
+        it "should work with urls that have .co.uk suffixes"
+        it "should work with urls that have .co.jp suffixes"
+      end
+    end
+  end
 end
 
