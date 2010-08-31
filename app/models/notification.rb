30940f4641593a35716a77c88d189b37d24e4a64
Jonathan Kupferman <jkupferman@umail.ucsb.edu>
2010-08-30 23:39:17 -0700

    Added error mailer for dealing with unhandled exceptions.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,12 +1,25 @@
 class Notification < ActionMailer::Base
-  CONTACT_EMAIL_ADDRESS = "contact@isitcloudy.com"
+  EMAIL_DOMAIN = "isitcloudy.com"
+  CONTACT_EMAIL = "contact@#{EMAIL_DOMAIN}"
+  ERROR_EMAIL = "error@#{EMAIL_DOMAIN}"
+  ERROR_NOTIFIER_EMAIL = "error@#{EMAIL_DOMAIN}"
 
   def contact email
     subject "[Contact Us] " << email[:subject]
-    recipients CONTACT_EMAIL_ADDRESS
+    recipients CONTACT_EMAIL
     from email[:email]
     sent_on Time.now.utc
 
     body :message => email[:body], :name => email[:name]
   end
+
+  def error exception, trace, session, params, env
+    subject "[Error] " << env['REQUEST_URI']
+    recipients ERROR_EMAIL
+    from ERROR_NOTIFIER_EMAIL
+    sent_on Time.now.utc
+
+    body :exception => exception, :trace => trace, :session => session, :params => params, :env => env
+  end
+
 end
