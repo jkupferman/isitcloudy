8548373e9d6b745d73d273c345115e27d2043f22
Jonathan Kupferman <jkupferman@umail.ucsb.edu>
2010-08-31 20:51:43 -0700

    Added catch-all exception handling and mailer notification.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < ActionController::Base
   layout "default"
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  rescue_from Exception, :with => :rescue_all_exceptions if Rails.env.production?
+
+  private
+  def rescue_all_exceptions(exception)
+    Notification.deliver_error(exception,
+                               clean_backtrace(exception),
+                               session,
+                               params,
+                               request.env
+                               )
+
+    render :file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404
+  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@ class Notification < ActionMailer::Base
   EMAIL_DOMAIN = "isitcloudy.com"
   CONTACT_EMAIL = "contact@#{EMAIL_DOMAIN}"
   ERROR_EMAIL = "error@#{EMAIL_DOMAIN}"
-  ERROR_NOTIFIER_EMAIL = "error@#{EMAIL_DOMAIN}"
+  ERROR_NOTIFIER_EMAIL = "error-notifier@#{EMAIL_DOMAIN}"
 
   def contact email
     subject "[Contact Us] " << email[:subject]
@@ -14,12 +14,11 @@ class Notification < ActionMailer::Base
   end
 
   def error exception, trace, session, params, env
-    subject "[Error] " << env['REQUEST_URI']
+    subject "[Error] " << env['REQUEST_URI'] << " " << exception.message
     recipients ERROR_EMAIL
     from ERROR_NOTIFIER_EMAIL
     sent_on Time.now.utc
 
     body :exception => exception, :trace => trace, :session => session, :params => params, :env => env
   end
-
 end
