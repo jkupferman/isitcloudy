fe05e4b6b9f330d8a51d1e1b59966a15fccf0cef
Jonathan Kupferman <jkupferman@umail.ucsb.edu>
2010-09-01 23:02:21 -0700

    Changed asset tag to use shortened hexdigest since to_i was unpredictable and could cause collisions.

--- lib/monkey_patches.rb
+++ lib/monkey_patches.rb
@@ -20,7 +20,7 @@ module ActionView
             asset_id
           else
             path = File.join(ASSETS_DIR, source)
-            asset_id = File.exist?(path) ? Digest::MD5.file(path).hexdigest.to_i(12) : ''
+            asset_id = File.exist?(path) ? Digest::MD5.file(path).hexdigest[0..6] : ''
             
             if @@cache_asset_timestamps
               @@asset_timestamps_cache_guard.synchronize do
