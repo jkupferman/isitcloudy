c1b1d95bbe36ec93e0bffc347db098d84c057e49
Jonathan Kupferman <jkupferman@umail.ucsb.edu>
2010-09-20 23:09:53 -0700

    Changed contact us page to cache-control private since it has a form.

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,5 +1,6 @@
 class HelpController < ApplicationController
-  before_filter :page_cacher, :only => [:about, :contact]
+  before_filter :page_cacher, :only => [:about]
+  before_filter :private_page_cacher, :only => [:contact]
 
   def about
   end
