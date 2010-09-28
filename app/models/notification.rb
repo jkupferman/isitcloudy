f11d672aa86a7197803e5a2919f94ac6f0f13a8f
Jonathan Kupferman <jkupferman@umail.ucsb.edu>
2010-09-27 20:44:21 -0700

    Changed contact template to contain the from address since Herkou's gmail plugin changes the from address to be the account address automatically.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,21 +1,21 @@
 class Notification < ActionMailer::Base
-  CONTACT_EMAIL        = "isitcloudywebsite+contact@gmail.com"
-  ERROR_EMAIL          = "isitcloudywebsite+error@gmail.com"
-  ERROR_NOTIFIER_EMAIL = "isitcloudywebsite+notifier@gmail.com"
+  CONTACT_EMAIL  = "isitcloudywebsite+contact@gmail.com"
+  ERROR_EMAIL    = "isitcloudywebsite+error@gmail.com"
+  NOTIFIER_EMAIL = "isitcloudywebsite@gmail.com"
 
   def contact email
     subject "[Contact Us] " << email[:subject]
     recipients CONTACT_EMAIL
-    from email[:email]
+    from NOTIFIER_EMAIL
     sent_on Time.now.utc
 
-    body :message => email[:body], :name => email[:name]
+    body :message => email[:body], :name => email[:name], :reply_to => email[:email]
   end
 
   def error exception, trace, session, params, env
     subject "[Error] " << env['REQUEST_URI'] << " " << exception.message
     recipients ERROR_EMAIL
-    from ERROR_NOTIFIER_EMAIL
+    from NOTIFIER_EMAIL
     sent_on Time.now.utc
 
     body :exception => exception, :trace => trace, :session => session, :params => params, :env => env
